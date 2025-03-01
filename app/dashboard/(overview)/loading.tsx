9f41b827b1ffe5b973d58fde0243a49580de89e2
Saifullah Khan <saifullahkhan42@gmail.com>
2025-03-01 21:46:41 +0530

    streaming chapter9 done

---
+++ app/dashboard/(overview)/loading.tsx
@@ -0,0 +1,9 @@
+import DashboardSkeleton from "@/app/ui/skeletons";
+export default function Loading() {
+  return (
+    <DashboardSkeleton />
+    // <div className="flex items-center justify-center h-screen">
+    //   <p className="text-2xl text-gray-400">Loading...</p>
+    // </div>
+  );
+}
--- app/lib/data.ts
+++ app/lib/data.ts
@@ -17,11 +17,11 @@ export async function fetchRevenue() {
     // Don't do this in production :)
 
     // console.log('Fetching revenue data...');
-    // await new Promise((resolve) => setTimeout(resolve, 3000));
+    await new Promise((resolve) => setTimeout(resolve, 3000));
 
     const data = await sql<Revenue[]>`SELECT * FROM revenue`;
 
-    // console.log('Data fetch completed after 3 seconds.');
+    console.log('Data fetch completed after 3 seconds.');
 
     return data;
   } catch (error) {
@@ -31,6 +31,7 @@ export async function fetchRevenue() {
 }
 
 export async function fetchLatestInvoices() {
+  await new Promise((resolve) => setTimeout(resolve, 5000)); // Simulate slow network
   try {
     const data = await sql<LatestInvoiceRaw[]>`
       SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
